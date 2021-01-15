a64cb95282d1925bbbeca1f2048620a3d84b4e49
Equipo 5 <arielsb.ti19@utsjr.edu.mx>
2021-01-15 08:59:13 -0600

    Preparar para servidor

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const path=require('path');
 const app=express();
+const port=process.env.port || 3000;
 
 app.use('/',express.static(path.join(__dirname,'/public')));
 
-app.listen(3000,()=>{
-    console.log("Servidor en el puerto 3000");
+app.listen(port,()=>{
+    console.log("Servidor en el puerto "+port);
 })
